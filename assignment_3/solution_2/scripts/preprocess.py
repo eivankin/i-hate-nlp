a6b2b6f2e83507e89e6c76a3ce82a535f532d5fc
Evgenij Ivankin <e.ivankin@innopolis.university>
2024-04-22 04:53:58 +0300

    more comments & fix style

--- assignment_3/solution_2/scripts/preprocess.py
+++ assignment_3/solution_2/scripts/preprocess.py
@@ -15,13 +15,28 @@ from tqdm import tqdm
 
 
 def main(
-        input_file: Path,
-        train_output: Path,
-        val_output: Path,
-        language: str = "ru",
-        val_split: float = 0.2,
-        span_key: str = "sc",
-):
+    input_file: Path,
+    train_output: Path,
+    val_output: Path,
+    language: str = "ru",
+    val_split: float = 0.2,
+    span_key: str = "sc",
+) -> None:
+    """
+    Preprocess the RuNNE data and save it to .spacy format.
+    1. Tokenize the data with tuned tokenizer
+    2. Change end boundaries (+1) so it is consistent with spacy format
+    3. Save information as span groups
+    4. Split to train and validation sets according to val_split value.
+    5. Save data on disk.
+
+    :param input_file: path to train.jsonl
+    :param train_output: path to save train set
+    :param val_output: path to save validation set
+    :param language: spacy tokenizer language, "ru" by default
+    :param val_split: fraction of a validation set, 20% by default
+    :param span_key: where to save span info (doc.spans[span_key]), "sc" by default
+    """
     nlp = spacy.blank(language)
 
     infixes = nlp.Defaults.infixes + [
@@ -49,15 +64,16 @@ def main(
     ]
     nlp.tokenizer.prefix_search = spacy.util.compile_prefix_regex(prefixes).search
 
+    # Make some punctuation be separate tokens by overriding default rules
     nlp.tokenizer.add_special_case("''", [{ORTH: "'"}, {ORTH: "'"}])
-    nlp.tokenizer.add_special_case(
-        "руб.", [{ORTH: "руб", NORM: "рубль"}, {ORTH: "."}]
-    )
+    nlp.tokenizer.add_special_case("руб.", [{ORTH: "руб", NORM: "рубль"}, {ORTH: "."}])
     nlp.tokenizer.add_special_case(
         "долл.", [{ORTH: "долл", NORM: "доллар"}, {ORTH: "."}]
     )
     nlp.tokenizer.add_special_case("г.", [{ORTH: "г"}, {ORTH: "."}])
     nlp.tokenizer.add_special_case("гг.", [{ORTH: "гг", NORM: "годы"}, {ORTH: "."}])
+
+    # Disable special rules regarding smileys
     nlp.tokenizer.add_special_case(":0", [{ORTH: ":"}, {ORTH: "0"}])
     nlp.tokenizer.add_special_case(":1", [{ORTH: ":"}, {ORTH: "1"}])
     nlp.tokenizer.add_special_case(":3", [{ORTH: ":"}, {ORTH: "3"}])
@@ -72,9 +88,9 @@ def main(
             for start, end, label in annotations:
                 span = doc.char_span(start, end + 1, label=label)
                 assert span is not None, (
-                    text[start - 5: start],
-                    text[start: end + 1],
-                    text[end + 1: end + 6],
+                    text[start - 5 : start],
+                    text[start : end + 1],
+                    text[end + 1 : end + 6],
                 )
                 spans.append(span)
             doc.spans[span_key] = SpanGroup(doc, name=span_key, spans=spans)
